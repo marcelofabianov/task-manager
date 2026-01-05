7823cccdcbf975cfca16c9408b4d176cae7511b7
Marcelo Fabiano <marcelofabianov@gmail.com>
2026-01-05 17:49:21 -0300

    refactor: ajustar repository

--- src/infrastructure/storage/db/postgres-user.repository.ts
+++ src/infrastructure/storage/db/postgres-user.repository.ts
@@ -3,7 +3,7 @@ import { UserRepository } from "@domain/repository/user.repository";
 import { Either } from "@main/shared/either";
 
 export class PostgresUserRepository implements UserRepository {
-  async create(user: User): Promise<Either<Error, User>> {
+  async save(user: User): Promise<Either<Error, User>> {
     throw new Error("Method not implemented.");
   }
 }
